55089e7abdfe7e875647731eecb5e7cadb85ad84
arhturfary <arthurfary22@gmail.com>
2024-01-25 20:22:40 -0300

    Framer Animations

--- app/components/background/backgroundElements.jsx
+++ app/components/background/backgroundElements.jsx
@@ -1,17 +1,28 @@
 import styles from "./background.module.css"
+import bulb from "../../../public/light-bulb-idea-svgrepo-com.svg"
+import Image from "next/image"
+import { motion } from "framer-motion"
 
 export default function BackgroundElements() {
+    function createAnimation(duration) {
+        return {
+          key: "test",
+          initial: {opacity: 1, y: 50},
+          whileInView: {opacity: 1, y: 0},
+          transition: {ease: "easeOut", duration: duration},
+          exit: {opacity: 1, x: -100},
+        };
+      }
+      
     return (
     <>
     <div className={styles.background}>
         <div className={styles.bgHero}>
-            <span className={`${styles.heroSun} ${styles.gridObject}`}></span> 
-
-            <span className={`${styles.heroMountainOne} ${styles.gridObject}`}></span>
-            <span className={`${styles.heroMountainTwo} ${styles.gridObject}`}></span>
-            
-            <span className={`${styles.heroMountainOverlap} ${styles.gridObject}`}></span>
-            <span className={`${styles.heroMountainOverlapTwo} ${styles.gridObject}`}></span>
+            <motion.span {...createAnimation(3)} initial={{opacity: 0, y: 200}} className={`${styles.heroSun} ${styles.gridObject}`}></motion.span> 
+            <motion.span {...createAnimation(2.5)} className={`${styles.heroMountainOne} ${styles.gridObject}`}></motion.span>
+            <motion.span {...createAnimation(2)} className={`${styles.heroMountainTwo} ${styles.gridObject}`}></motion.span>
+            <motion.span {...createAnimation(1)} className={`${styles.heroMountainOverlap} ${styles.gridObject}`}></motion.span>
+            <motion.span {...createAnimation(0.8)} className={`${styles.heroMountainOverlapTwo} ${styles.gridObject}`}></motion.span>
         </div>
 
         <div className={styles.bgAboutMe}>
@@ -21,8 +32,8 @@ export default function BackgroundElements() {
         </div>
 
         <div className={styles.bgImagine}>
-            <span className={`${styles.imagineSun} ${styles.gridObject}`}></span> 
-
+            {/* <span className={`${styles.imagineSun} ${styles.gridObject}`}></span>  */}
+            <Image className={`${styles.bulb} ${styles.gridObject}`} src={bulb} />
         </div>
     </div>
     </>
--- app/page.jsx
+++ app/page.jsx
@@ -1,49 +1,65 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import PageLayout from './pageLayout'
+import { motion } from "framer-motion"
 
 export default function Home() {
+
+  function createAnimation(duration, delay, direction = "x", startFrom = -100) {
+    return {
+      initial: {opacity: 0, [direction]: startFrom},
+      whileInView: {opacity: 1, x: 0, y: 0},
+      transition: {duration: duration, delay: delay},
+    };
+  }
+  
   return (
     <PageLayout>
       <div className={styles.startHero}>
-        <div className={styles.titleHero}>
-            <h1>Oi, me chamo</h1>
-            <h1>Arthur</h1>
+        <motion.div className={styles.titleHero}>
+          <motion.h1 {...createAnimation(2, 0, "y")}>Oi, me chamo</motion.h1>
+          <motion.h1 {...createAnimation(2, 0, "y")}>Arthur</motion.h1>
           <p>E seja bem vindo ao meu site</p>
-
+  
           <a href="" className={styles.buttonHero}>
             Entrar em Contato
           </a>
-        </div>
+        </motion.div>
       </div>
-
+  
       <div className={styles.aboutMe}>
-          <div className={styles.textCard}>
-            <h1>Programador Full-Stack</h1>
-          </div>
-          <div className={styles.textCard}>
-            <h1>JavaScript</h1>
-            <p>NextJS - React - NodeJS - Router - APIs - JSX</p>
-          </div>
-          <div className={styles.textCard}>
-            <h1>Python</h1>
-            <p>Pandas - Selenium - Discord - SQLite3</p>
-          </div>
-          <div className={styles.textCard}>
-            <p>I also know a bit of</p>
-            <h2>C; C#; C++; PHP; LUA</h2>
-          </div>
-          <div className={styles.textCard}>
-            <p>And I pride myself with</p>
-            <h2>Clean Code; OOP</h2>
-          </div>
+        <motion.div {...createAnimation(1, 0)} className={styles.textCard}>
+          <h1>Programador Full-Stack</h1>
+        </motion.div>
+  
+        <motion.div {...createAnimation(1, 0)} className={styles.textCard}>
+          <h1>JavaScript</h1>
+          <motion.p {...createAnimation(1, 0.2)}>NextJS React - NodeJS - Router - APIs - JSX</motion.p>
+        </motion.div>
+  
+        <motion.div {...createAnimation(1, 0)} className={styles.textCard}>
+          <h1>Python</h1>
+          <motion.p {...createAnimation(1, 0.2)}>Pandas - Selenium - Discord - SQLite3</motion.p>
+        </motion.div>
+  
+        <motion.div {...createAnimation(1, 0)} className={styles.textCard}>
+          <motion.p {...createAnimation(1, 0.2)}>I also know a bit of</motion.p>
+          <h2>C; C#; C++; PHP; LUA</h2>
+        </motion.div>
+  
+        <motion.div {...createAnimation(1, 0)} className={styles.textCard}>
+          <motion.p {...createAnimation(1, 0.2)}>And I pride myself with</motion.p>
+          <h2>Clean Code; OOP</h2>
+        </motion.div>
       </div>
-
+  
       <div className={styles.imagine}>  
         <h1>Você imagina, Eu construo!</h1>
-
+  
         {/* BIG SUN WITH LAKE HERE, TALKING ABOUT HOW YOU DREAM AND I BUILD, MAYBE THE SCENE FROM KRO WERE THE METEOR LANDS */}
-
+  
       </div>
     </PageLayout>
   )
